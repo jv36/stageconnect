6fcfb9139538eb3a7ed09b19bf10bf8fa3452207
João Vicente <up202208586@up.pt>
2025-05-02 12:47:27 +0100

    hhh

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -5,13 +5,11 @@ import XIcon from '@mui/icons-material/X';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 
-interface PageProps {
-    params: {
-      id: string
-    }
-}
-
-export default async function UserProfilePage({ params }: PageProps) {
+export default async function UserProfilePage({
+    params,
+  }: {
+    params: { id: string }
+  }) {
   const supabase = await createClient();
 
   // Query the public metadata of a user by ID
