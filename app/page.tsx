a76364f43105b76752a720d6352feded8d6c0bec
João Vicente <up202208586@up.pt>
2025-05-02 20:26:02 +0100

    Fallback

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-'use client'
+'use client';
 import Carousel from "@/components/Carousel";
-import MyCard from "@/components/MyCard";
 import allCountryCodes from "@/utils/countryCodes";
 import { Typography } from "@mui/material";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from 'react';
+
+function LoadingCarousel() {
+  return <div>Loading concerts...</div>; // Simple loading indicator
+}
 
 export default function Home() {
   const searchParams = useSearchParams();
   const countryParam = searchParams.get('country');
-
-  // Determine the country code directly from the URL parameter,
-  // defaulting to 'CZ' if no valid parameter is found.
   const currentCountryCode: string =
     countryParam && allCountryCodes.some(c => c.code === countryParam)
       ? countryParam
@@ -20,16 +21,24 @@ export default function Home() {
     <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-roboto-condensed)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full">
         <Typography variant="h5" color="primary" fontWeight={600}>Popular in {currentCountryCode}</Typography>
-        <Carousel countryCode={currentCountryCode} />
+        <Suspense fallback={<LoadingCarousel />}>
+          <Carousel countryCode={currentCountryCode} />
+        </Suspense>
 
         <Typography variant="h5" color="primary" fontWeight={600}>Fancy a Pop concert?</Typography>
-        <Carousel countryCode={currentCountryCode} />
+        <Suspense fallback={<LoadingCarousel />}>
+          <Carousel countryCode={currentCountryCode} />
+        </Suspense>
 
         <Typography variant="h5" color="primary" fontWeight={600} >Global Stars</Typography>
-        <Carousel countryCode={currentCountryCode} />
+        <Suspense fallback={<LoadingCarousel />} >
+          <Carousel countryCode={currentCountryCode} />
+        </Suspense>
 
         <Typography variant="h5" color="primary" fontWeight={600}>Big Venues</Typography>
-        <Carousel countryCode={currentCountryCode} />
+        <Suspense fallback={<LoadingCarousel />}>
+          <Carousel countryCode={currentCountryCode} />
+        </Suspense>
       </main>
     </div>
   );
